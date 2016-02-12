c0ed090baf36a44750a6270389722e08c06d93a8
shameera <shameerainfo@gmail.com>
2016-02-12 01:39:38 -0500

    Added hadoop page rank map and reducer codes

--- HadoopPageRank/src/indiana/cgl/hadoop/pagerank/PageRankMap.java
+++ HadoopPageRank/src/indiana/cgl/hadoop/pagerank/PageRankMap.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import java.util.ArrayList;
+
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -12,32 +13,36 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Mapper;
- 
+
 public class PageRankMap extends Mapper<LongWritable, Text, LongWritable, Text> {
 
-// each map task handles one line within an adjacency matrix file
+    // each map task handles one line within an adjacency matrix file
 // key: file offset
 // value: <sourceUrl PageRank#targetUrls>
-	public void map(LongWritable key, Text value, Context context)
-		throws IOException, InterruptedException {
-			int numUrls = context.getConfiguration().getInt("numUrls",1);
-			String line = value.toString();
-			StringBuffer sb = new StringBuffer();
-			// instance an object that records the information for one webpage
-			RankRecord rrd = new RankRecord(line);
-			int sourceUrl, targetUrl;
-			// double rankValueOfSrcUrl;
-			if (rrd.targetUrlsList.size()<=0){
-				// there is no out degree for this webpage; 
-				// scatter its rank value to all other urls
-				double rankValuePerUrl = rrd.rankValue/(double)numUrls;
-				for (int i=0;i<numUrls;i++){
-				context.write(new LongWritable(i), new Text(String.valueOf(rankValuePerUrl)));
-				}
-			} else {
-				/*Write your code here*/
-			} //for
-			context.write(new LongWritable(rrd.sourceUrl), new Text(sb.toString()));
-		} // end map
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        int numUrls = context.getConfiguration().getInt("numUrls", 1);
+        String line = value.toString();
+        StringBuffer sb = new StringBuffer();
+        // instance an object that records the information for one webpage
+        RankRecord rrd = new RankRecord(line);
+
+        if (rrd.targetUrlsList.size() <= 0) {
+            // Dangling nodes(web pages) scatter its rank value to all other urls
+            double rankValuePerUrl = rrd.rankValue / (double) numUrls;
+            for (int i = 0; i < numUrls; i++) {
+                context.write(new LongWritable(i), new Text(String.valueOf(rankValuePerUrl)));
+                sb.append("#");
+            }
+        } else {
+                /*Write your code here*/
+            double rankValue = rrd.rankValue / ((double) rrd.targetUrlsList.size());
+            for (int targetUrl : rrd.targetUrlsList) {
+                context.write(new LongWritable(targetUrl), new Text(String.valueOf(rankValue)));
+                sb.append("#").append(targetUrl);
+            }
+        } //for
+        context.write(new LongWritable(rrd.sourceUrl), new Text(sb.toString()));
+    } // end map
 
 }
--- HadoopPageRank/src/indiana/cgl/hadoop/pagerank/PageRankReduce.java
+++ HadoopPageRank/src/indiana/cgl/hadoop/pagerank/PageRankReduce.java
@@ -28,10 +28,13 @@ public class PageRankReduce extends Reducer<LongWritable, Text, LongWritable, Te
 		
 		//hints each tuple may include: rank value tuple or link relation tuple  
 		for (Text value: values){
-			String[] strArray = value.toString().split("#");
-
 			/*Write your code here*/
-
+			String str = value.toString();
+			if (str.charAt(0) == '#') {
+				targetUrlsList = str;
+			} else {
+				sumOfRankValues += Double.valueOf(str);
+			}
 		} // end for loop
 		sumOfRankValues = 0.85*sumOfRankValues+0.15*(1.0)/(double)numUrls;
 		context.write(key, new Text(sumOfRankValues+targetUrlsList));
