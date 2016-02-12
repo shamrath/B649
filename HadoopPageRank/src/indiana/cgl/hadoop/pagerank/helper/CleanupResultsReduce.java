66e0fae50cf9c4780b79ee23c55003a997eb9994
shameera <shameerainfo@gmail.com>
2016-02-12 01:46:14 -0500

    Calculate summation of all page ranks at cleanup results reducer

--- HadoopPageRank/src/indiana/cgl/hadoop/pagerank/helper/CleanupResultsReduce.java
+++ HadoopPageRank/src/indiana/cgl/hadoop/pagerank/helper/CleanupResultsReduce.java
@@ -7,9 +7,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class CleanupResultsReduce extends Reducer<LongWritable, Text, LongWritable, Text>{
-public void reduce(LongWritable key, Iterable<Text> values,
-		Context context) throws IOException, InterruptedException {
-	context.write(key, values.iterator().next());
-}
+public class CleanupResultsReduce extends Reducer<LongWritable, Text, LongWritable, Text> {
+
+    private static int count = 0;
+    private static double prSum = 0.0;
+    public void reduce(LongWritable key, Iterable<Text> values,
+                       Context context) throws IOException, InterruptedException {
+        int numUrls = context.getConfiguration().getInt("numUrls", 1);
+        Text value = values.iterator().next();
+        prSum += Double.valueOf(value.toString());
+        count++;
+        context.write(key, values.iterator().next());
+        if (count == numUrls) {
+            System.out.println("Page Ranks sum :" + prSum);
+        }
+    }
 }
